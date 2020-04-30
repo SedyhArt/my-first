02bcc6a946e0286e610ca7b34d4705d3a74b78b5
SedyhArt <62831412+SedyhArt@users.noreply.github.com>
2020-04-30 12:57:11 +0300

    Add a theme change to the skills block

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
+let current_level1 = document.querySelector('.current-level1');
+let current_level2 = document.querySelector('.current-level2')
 
 themeButton.onclick = function() {
 page.classList.toggle('light-theme');
+current_level1.classList.toggle('light-theme');
+current_level2.classList.toggle('light-theme');
 };
 
